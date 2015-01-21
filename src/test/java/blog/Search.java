a00f6d6ba7c9821391215746c8592adff090fe75
Lana Voit <lanavoit@gmail.com>
2015-01-21 16:23:26 +0300

    test

--- src/test/java/blog/Search.java
+++ src/test/java/blog/Search.java
@@ -27,6 +27,7 @@ public class Search extends turnkeye2.pages.TestBase {
       driver.findElement(By.id("gsc-i-id1")).clear();
       driver.findElement(By.id("gsc-i-id1")).sendKeys("ТЕСТИРОВАНИЕ");
       driver.findElement(By.cssSelector("input.gsc-search-button.gsc-search-button-v2")).click();
+      TimeUnit.SECONDS.sleep(5);
       assertEquals("Юзабилити-тестирование: как увеличить прибыльность ...", driver.findElement(By.linkText("Юзабилити-тестирование: как увеличить прибыльность ...")).getText());
       driver.findElement(By.cssSelector("div.gsc-results-close-btn.gsc-results-close-btn-visible")).click();
       assertEquals("Статьи", driver.findElement(By.cssSelector("h1")).getText());
