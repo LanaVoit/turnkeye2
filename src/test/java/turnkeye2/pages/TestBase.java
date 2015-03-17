a94a127dd2da090f838eeb043b4ae8d3d2243119
Lana Voit <lanavoit@gmail.com>
2015-03-17 09:52:44 +0300

    test

--- src/test/java/turnkeye2/pages/TestBase.java
+++ src/test/java/turnkeye2/pages/TestBase.java
@@ -94,7 +94,7 @@ public class TestBase {
 		String username = PropertyLoader.loadProperty("user.username");
 		String password = PropertyLoader.loadProperty("user.password");
 		
-		 DesiredCapabilities capabillities = DesiredCapabilities.chrome();
+		/* DesiredCapabilities capabillities = DesiredCapabilities.chrome();
 	        capabillities.setCapability("version", "39.0");
 	        capabillities.setCapability("platform", Platform.WIN8);	
 	        capabillities.setCapability("screen-resolution", "1280x1024");
@@ -103,7 +103,7 @@ public class TestBase {
 	        driver = new RemoteWebDriver(
 	                    new URL("http://qatestingtestqa:7d7d449c-27c0-45c7-9339-3aad563a5cc0@ondemand.saucelabs.com:80/wd/hub"),
 	                    capabillities);
-
+*/
 	}  
 	  
 	
