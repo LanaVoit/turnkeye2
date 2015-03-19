b67a59c61954c15c0285ea96245f801745c11187
Lana Voit <lanavoit@gmail.com>
2015-03-19 15:53:58 +0300

    test

--- src/test/java/turnkeye2/pages/TestBase.java
+++ src/test/java/turnkeye2/pages/TestBase.java
@@ -94,6 +94,8 @@ public class TestBase {
 		String username = PropertyLoader.loadProperty("user.username");
 		String password = PropertyLoader.loadProperty("user.password");
 		
+		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/tools/chromedriver");
+		
 		DesiredCapabilities capabillities = DesiredCapabilities.chrome();
 	        //capabillities.setCapability("platform", Platform.LINUX);
 	        capabillities.setCapability("screen-resolution", "1280x1024");
