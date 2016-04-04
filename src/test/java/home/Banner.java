2668e9ea37a09539d60dae498f65f8ce31bb01b4
LanaVoit <lanavoit@gmail.com>
2016-04-04 17:46:06 +0400

    home

--- src/test/java/home/Banner.java
+++ src/test/java/home/Banner.java
@@ -24,18 +24,21 @@ public class Banner extends turnkeye2.pages.TestBase {
     
     driver.get(baseUrl);
 
-    String img1 = "http://cdn.astrio.ru/skin/frontend/default/turnkeye/images/main-banner/large/slide1-moscow.jpg";
-    assertEquals("", img1, driver.findElement(By.cssSelector("img[src=\"http://cdn.astrio.ru/skin/frontend/default/turnkeye/images/main-banner/large/slide1-moscow.jpg\"]")).getAttribute("src"));  
+    //String img1 = "http://cdn.astrio.ru/skin/frontend/default/turnkeye/images/main-banner/large/slide1-moscow.jpg";
+    String img1 = "http://astrio.ru/skin/frontend/default/turnkeye/images/main-banner/large/2000x520xslide1-moscow.jpg.pagespeed.ic.6QQP1U0tX-.webp";
+    assertEquals("", img1, driver.findElement(By.cssSelector("img[src=\"http://astrio.ru/skin/frontend/default/turnkeye/images/main-banner/large/2000x520xslide1-moscow.jpg.pagespeed.ic.6QQP1U0tX-.webp\"]")).getAttribute("src"));  
 
     
     driver.findElement(By.cssSelector("img[alt=\"Estet\"]")).click();
-    String img2 = "http://cdn.astrio.ru/media/wysiwyg//home-page/slide-estet-0001.jpg";
-    assertEquals("", img2, driver.findElement(By.cssSelector("img[src=\"http://cdn.astrio.ru/media/wysiwyg//home-page/slide-estet-0001.jpg\"]")).getAttribute("src")); 
+    //String img2 = "http://cdn.astrio.ru/media/wysiwyg//home-page/slide-estet-0001.jpg";
+    String img2 = "http://astrio.ru/media/wysiwyg/main-banner/home-original/Nx520xslide-estet.jpg.pagespeed.ic.a1zdO-4m4e.webp";
+    assertEquals("", img2, driver.findElement(By.cssSelector("img[src=\"http://astrio.ru/media/wysiwyg/main-banner/home-original/Nx520xslide-estet.jpg.pagespeed.ic.a1zdO-4m4e.webp\"]")).getAttribute("src")); 
     
     TimeUnit.SECONDS.sleep(5);
     driver.findElement(By.cssSelector("img[alt=\"Autoexpert\"]")).click();
-    String img4 = "http://cdn.astrio.ru/media/wysiwyg//home-page/slide-autoexpert-001.jpg";
-    assertEquals("", img4, driver.findElement(By.cssSelector("img[src=\"http://cdn.astrio.ru/media/wysiwyg//home-page/slide-autoexpert-001.jpg\"]")).getAttribute("src")); 
+    //String img4 = "http://cdn.astrio.ru/media/wysiwyg//home-page/slide-autoexpert-001.jpg";
+    String img4 = "http://astrio.ru/media/wysiwyg/main-banner/home-original/Nx520xslide-autoexpert.jpg.pagespeed.ic.Yjxfai3-Cu.webp";
+    assertEquals("", img4, driver.findElement(By.cssSelector("img[src=\"http://astrio.ru/media/wysiwyg/main-banner/home-original/Nx520xslide-autoexpert.jpg.pagespeed.ic.Yjxfai3-Cu.webp\"]")).getAttribute("src")); 
   }
 
     private boolean isElementPresent(By by) {
