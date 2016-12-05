d7f4cc2cec1bcd3a6e34c0e2925ead2c8f0bbb14
LanaVoit <lanavoit@gmail.com>
2016-12-05 12:59:42 +0400

    User-Agent

--- src/test/java/clients/Link_visit_website.java
+++ src/test/java/clients/Link_visit_website.java
@@ -62,6 +62,7 @@ public class Link_visit_website extends turnkeye2.pages.TestBase{
 	    		URL url = new URL(urlString);
 		        HttpURLConnection huc = (HttpURLConnection)url.openConnection();
 		        huc.setRequestMethod("GET");
+		        huc.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");			       
 		        huc.connect();
 		        return huc.getResponseCode();
 	    	} catch (Exception e) {
